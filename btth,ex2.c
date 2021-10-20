f826a22773e4a278969a8f9c51917ab81a3c7179
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-10-20 09:41:31 +0700

    Add files via upload

---
+++ btth,ex2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i,mang[10];
+	for(i=0;i<10;i++)
+	{
+		printf("Nhap so thu %d: ",i+1);
+		scanf("%d",&mang[i]);
+	}
+	printf("\nKet qua: ");
+	for(i=9;i>=0;i--)
+	{
+		printf("%d, ",mang[i]);
+	}
+	
+	printf("\n\nThank you <3");
+	return 0;
+}
